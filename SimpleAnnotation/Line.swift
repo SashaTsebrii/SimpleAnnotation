40af6050c797602edce8f1d984df303a5db0ca98
SashaTsebrii <sashatsebrii@gmail.com>
2020-06-29 17:07:03 +0300

    Implement Line struct.

---
+++ SimpleAnnotation/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  SimpleAnnotation
+//
+//  Created by Aleksandr Tsebrii on 6/29/20.
+//  Copyright © 2020 Aleksandr Tsebrii. All rights reserved.
+//
+
+import UIKit
+
+struct Line {
+    let strokeWidth: Float
+    let color: UIColor
+    var points: [CGPoint]
+}
