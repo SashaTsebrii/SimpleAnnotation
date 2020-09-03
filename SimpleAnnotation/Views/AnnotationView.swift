729817b38cb981f6a54b8f620c5b57da4a0c81a3
SashaTsebrii <sashatsebrii@gmail.com>
2020-09-03 20:28:52 +0300

    Implement AnnotationView.

---
+++ SimpleAnnotation/Views/AnnotationView.swift
@@ -0,0 +1,13 @@
+//
+//  AnnotationView.swift
+//  SimpleAnnotation
+//
+//  Created by Aleksandr Tsebrii on 9/3/20.
+//  Copyright © 2020 Aleksandr Tsebrii. All rights reserved.
+//
+
+import UIKit
+
+class AnnotationView: UIView {
+    
+}
