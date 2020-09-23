1bee26c7c86252f2bca96df7391d369326d868cb
SashaTsebrii <sashatsebrii@gmail.com>
2020-09-23 20:55:30 +0300

    Move gesture logic to from AnnotationController to StickerImageView.

--- SimpleAnnotation/Views/StickerImageView.swift
+++ SimpleAnnotation/Views/StickerImageView.swift
@@ -10,4 +10,64 @@ import UIKit
 
 class StickerImageView: EditableImageView {
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        let panGesture = UIPanGestureRecognizer()
+        panGesture.addTarget(self, action: #selector(handlePanGesture(_:)))
+        
+        let pinchGesture = UIPinchGestureRecognizer()
+        pinchGesture.addTarget(self, action: #selector(handlePinchGesture(_:)))
+        
+        let rotationGesture = UIRotationGestureRecognizer()
+        rotationGesture.addTarget(self, action: #selector(handleRotateGesture(_:)))
+        
+        addGestureRecognizer(panGesture)
+        addGestureRecognizer(pinchGesture)
+        addGestureRecognizer(rotationGesture)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc func handlePanGesture(_ gesture: UIPanGestureRecognizer) {
+        print("👋 PAN GESTURE IN STICKER IMAGE VIEW")
+        
+        let translation = gesture.translation(in: superview)
+        
+        guard let gestureView = gesture.view else {
+            return
+        }
+        
+        gestureView.center = CGPoint(x: gestureView.center.x + translation.x, y: gestureView.center.y + translation.y)
+        gesture.setTranslation(.zero, in: superview)
+        
+    }
+    
+    @objc func handlePinchGesture(_ gesture: UIPinchGestureRecognizer) {
+        print("👋 PINCH GESTURE IN STICKER IMAGE VIEW")
+        
+        guard let gestureView = gesture.view else {
+            return
+        }
+        
+        gestureView.transform = gestureView.transform.scaledBy(x: gesture.scale, y: gesture.scale)
+        gesture.scale = 1
+        
+    }
+    
+    @objc func handleRotateGesture(_ gesture: UIRotationGestureRecognizer) {
+        print("👋 ROTATE GESTURE IN STICKER IMAGE VIEW")
+        
+        guard let gestureView = gesture.view else {
+            return
+        }
+        
+        gestureView.transform = gestureView.transform.rotated(by: gesture.rotation)
+        gesture.rotation = 0
+        
+    }
+    
 }
