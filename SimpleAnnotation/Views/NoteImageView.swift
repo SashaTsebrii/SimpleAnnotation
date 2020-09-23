b5ae0f6e72ed4b801c76b99ed2f45ab3ae3dd9a7
SashaTsebrii <sashatsebrii@gmail.com>
2020-09-23 20:54:56 +0300

    Move gesture logic to from AnnotationController to NoteImageView.

--- SimpleAnnotation/Views/NoteImageView.swift
+++ SimpleAnnotation/Views/NoteImageView.swift
@@ -8,6 +8,48 @@
 
 import UIKit
 
+
+
 class NoteImageView: EditableImageView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        let panGesture = UIPanGestureRecognizer()
+        panGesture.addTarget(self, action: #selector(handlePanGesture(_:)))
+        addGestureRecognizer(panGesture)
+        
+        let tapGesture = UITapGestureRecognizer()
+        tapGesture.addTarget(self, action: #selector(handleTapGesture(_:)))
+        addGestureRecognizer(tapGesture)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc fileprivate func handleTapGesture(_ gesture: UITapGestureRecognizer) {
+        print("👋 TAP GESTURE IN NOTE IMAGE VIEW")
+
+//        let enterTextController = EnterTextController()
+//        enterTextController.delegate = self
+//        navigationController?.pushViewController(enterTextController, animated: true)
+
+    }
+    
+    @objc func handlePanGesture(_ gesture: UIPanGestureRecognizer) {
+        print("👋 PAN GESTURE IN NOTE IMAGE VIEW")
+        
+        let translation = gesture.translation(in: superview)
+        
+        guard let gestureView = gesture.view else {
+            return
+        }
+        
+        gestureView.center = CGPoint(x: gestureView.center.x + translation.x, y: gestureView.center.y + translation.y)
+        gesture.setTranslation(.zero, in: superview)
+        
+    }
     
 }
