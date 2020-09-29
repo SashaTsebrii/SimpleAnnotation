718ae7f08f03cd1b8cfaf9cfcc6435da36baed41
SashaTsebrii <sashatsebrii@gmail.com>
2020-09-29 18:45:40 +0300

    Implement Note structure.

---
+++ SimpleAnnotation/Structures/Note.swift
@@ -0,0 +1,16 @@
+//
+//  Note.swift
+//  SimpleAnnotation
+//
+//  Created by Aleksandr Tsebrii on 9/29/20.
+//  Copyright © 2020 Aleksandr Tsebrii. All rights reserved.
+//
+
+import UIKit
+
+struct Note {
+    let date: Date
+    let text: String
+    let color: UIColor
+    let point: CGPoint
+}
