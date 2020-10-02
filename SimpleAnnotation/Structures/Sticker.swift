74710fd679067fb72fce79c3f8f51b3f4ce26973
SashaTsebrii <sashatsebrii@gmail.com>
2020-10-02 22:38:38 +0300

    Implement Sticker structure.

---
+++ SimpleAnnotation/Structures/Sticker.swift
@@ -0,0 +1,15 @@
+//
+//  Sticker.swift
+//  SimpleAnnotation
+//
+//  Created by Aleksandr Tsebrii on 10/2/20.
+//  Copyright © 2020 Aleksandr Tsebrii. All rights reserved.
+//
+
+import UIKit
+
+struct Sticker {
+    let date: Date
+    let name: String
+    let rect: CGRect
+}
