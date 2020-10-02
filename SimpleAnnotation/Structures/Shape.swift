e63682a143ae7bdf7c9873542649013e1d1812d3
SashaTsebrii <sashatsebrii@gmail.com>
2020-10-02 22:38:08 +0300

    Implement Shape structure.

---
+++ SimpleAnnotation/Structures/Shape.swift
@@ -0,0 +1,16 @@
+//
+//  Shape.swift
+//  SimpleAnnotation
+//
+//  Created by Aleksandr Tsebrii on 10/2/20.
+//  Copyright © 2020 Aleksandr Tsebrii. All rights reserved.
+//
+
+import UIKit
+
+struct Shape {
+    let date: Date
+    let shape: Shapes
+    let color: UIColor
+    let rect: CGRect
+}
