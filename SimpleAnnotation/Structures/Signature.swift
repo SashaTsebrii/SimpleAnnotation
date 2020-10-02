16d68f45a90dd0d48ec6c61fe163d901f98151c8
SashaTsebrii <sashatsebrii@gmail.com>
2020-10-02 22:38:23 +0300

    Implement Signature structure.

---
+++ SimpleAnnotation/Structures/Signature.swift
@@ -0,0 +1,15 @@
+//
+//  Signature.swift
+//  SimpleAnnotation
+//
+//  Created by Aleksandr Tsebrii on 10/2/20.
+//  Copyright © 2020 Aleksandr Tsebrii. All rights reserved.
+//
+
+import UIKit
+
+struct Signature {
+    let date: Date
+    let point: [CGPoint]
+    let rect: CGRect
+}
