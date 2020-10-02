1c3356cf84836b413fa699c4248646555d0264d9
SashaTsebrii <sashatsebrii@gmail.com>
2020-10-02 22:38:51 +0300

    Implement Text structure.

---
+++ SimpleAnnotation/Structures/Text.swift
@@ -0,0 +1,15 @@
+//
+//  Text.swift
+//  SimpleAnnotation
+//
+//  Created by Aleksandr Tsebrii on 10/2/20.
+//  Copyright © 2020 Aleksandr Tsebrii. All rights reserved.
+//
+
+import UIKit
+
+struct Text {
+    let date: Date
+    let text: String
+    let point: CGPoint
+}
